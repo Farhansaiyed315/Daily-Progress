712d9429bd341645b430ab513ee449560164520f
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-06-13 00:44:13 +0530

    email check

--- Exercise/script.js
+++ Exercise/script.js
@@ -1,15 +1,20 @@
 console.log("Hello, World!"); 
+//! Declare an array with more than 5 number of elements
 
-const express = require('express')
-const app = express()
-const port = 3000
+let numbner = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//! Use the forEach method to iterate over the array and print each element
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+
+
+
+
+
+
+
+
+
 
 
