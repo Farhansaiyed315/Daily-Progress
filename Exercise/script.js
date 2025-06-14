68f4f672617f0aa9d1cfafd6b2157d4ab312965b
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-06-14 21:59:15 +0530

    enviroment setup completed

--- Exercise/script.js
+++ Exercise/script.js
@@ -1,13 +1,3 @@
-console.log("Hello, World!"); 
-//! Declare an array with more than 5 number of elements
-
-let numbner = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//! Use the forEach method to iterate over the array and print each element
-
-
-
-
-
 
 
 
