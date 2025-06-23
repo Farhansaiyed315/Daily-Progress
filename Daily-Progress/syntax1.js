43758dd268fa5270a85acb148192e076ae0d2d30
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-06-23 22:51:32 +0530

    Daily-Progress

---
+++ Daily-Progress/syntax1.js
@@ -0,0 +1,50 @@
+
+let arr = [1, 2, 3]
+console.log(arr)
+
+let name = "My name is farhan"
+console.log(name)
+
+let obj = { name: "farhan", age: 23 }
+console.log(obj)
+
+if (5 == '5') {
+    console.log("True")
+
+}
+else {
+    console.log("False")
+
+}
+
+if (5 === '5') {
+    console.log("True")
+
+}
+
+else {
+    console.log(
+        "False"
+    )
+}
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
+
+let day = "Mon";
+
+switch (day) {
+  case "Mon":
+    console.log("Work");
+    break;
+
+  case "Sun":
+    console.log("Rest");
+    break;
+
+  default:
+    console.log("Invalid");
+    break;
+}
