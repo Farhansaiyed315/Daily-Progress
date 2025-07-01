5057335f81744ab52a7f7b3156e2bf88ebd1aef7
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-07-02 02:28:07 +0530

    Daily progress

---
+++ Exercise/String/String.js
@@ -0,0 +1,24 @@
+// ! 1. What is a string in JavaScript? How do you declare it?
+
+// let name1 = "Farhan";       // Double quotes
+// let name2 = 'Saiyed';       // Single quotes
+// let greeting = `Hello`;     // Backticks (template literal)
+
+
+//! 2. What is the difference between single quotes (''), double quotes (""), and backticks (``)?
+
+// let name = "Farhan";
+// let message = `Hello, my name is ${name}!`;
+// console.log(message); // Output: Hello, my name is Farhan!
+
+
+//! 3. How do you find the length of a string?
+
+// let name = "Farhan";
+// console.log(name.length);  // Output: 6
+
+// let name = "Farhan";
+
+// console.log(name[0]);  // F
+// console.log(name[1]);  // a
+// console.log(name[5]);  // n
