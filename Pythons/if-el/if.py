e51e22016e4c60bf6bbcb5988282529accd0ade9
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-07-03 19:04:48 +0530

    Daily progress

---
+++ Pythons/if-el/if.py
@@ -0,0 +1,24 @@
+ 
+# age = int(input("Enter your age: "))
+    
+# if age > 18 and age < 60:
+#     print("You can drive.")
+# elif age >= 60:
+#     print(" You can't drive. You're too old for it.")
+# else:
+# #     print(" You should wait. You're too young to drive.")
+
+# age = int(input("Enter the age of marriage"))
+
+# if age == 29 or age == 30:
+#     print("Shaadi ho jayegi bhidu")
+# else:
+#     print("Shaaadi nahi hogi") 
+    
+    
+num = int(input("Enter number to check wheather a number is prime or not"))
+if num % 2 == 0 :
+    print(num,"is an even number")
+else:
+    print(num,"is not a odd number")
+    
