75ab14e6fea7c57de78e4d01f463b96f43bbbe84
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-07-04 15:20:46 +0530

    Daily progress

--- Pythons/loops/loop.py
+++ Pythons/loops/loop.py
@@ -1,2 +1,35 @@
-for i in range(1,11):
-    print(i)
+
+# this is whiel loop 
+j = 1
+while j <= 10:   
+    print(j)
+    j+=1
+    
+name = 'Farhan'
+for i in name:
+        print(i)
+        if(i=="n"):
+            print("This is jhakaas")
+    
+colors = ["red", "Green","blue"]
+for colors in colors:
+        print(colors)
+        for i in colors:
+            print(i)
+            
+            
+            
+            # range in loops 
+            # if used only with  one argument it bydefault set start to zero
+            # with two arguments well you know the rest it will start from your given range and end at your given range or u can say arguents. 
+            
+            
+for k in range(1 , 9):
+    print("This is k ka print",k)
+    
+for s in range(0, 100, 5):
+    print("This is s ka print", s)
+    
+    
+        
+                  
