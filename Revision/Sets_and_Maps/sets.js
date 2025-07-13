4d107f3e576d1014ace89f0b98d6974ef96cb88e
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-07-13 15:44:48 +0530

    Daily progress

--- Revision/Sets_and_Maps/sets.js
+++ Revision/Sets_and_Maps/sets.js
@@ -86,3 +86,72 @@ const colors = new Set(["red", "green", "blue"]);
 
 console.log(colors.has("green")); 
 console.log(colors.has("yellow")); 
+
+
+//! 6. How can you find the size (number of elements) of a Set?
+
+
+
+const languages = new Set(["JavaScript", "Python", "C++", "Java"]);
+
+console.log(languages.size); 
+
+languages.add("Java");  
+console.log("duplicate, won't increase size",languages.size); 
+
+
+
+//! 7. What happens if you add duplicate values to a Set?
+
+// A Set only stores unique values.
+// If you try to add a duplicate, it will be ignored — no error, no change.
+
+
+
+//! 8. How can you loop through all the elements in a Set?
+
+
+const animals = new Set(["dog","cat","elephant"]);
+
+// Method 1: using for of loop
+
+// for (const animal of animals){
+//     console.log(animal)
+// }
+
+
+animals.forEach((animal) => {
+    console.log(animal)
+})
+
+
+
+
+
+//! 9. How do you convert a Set to an array?
+
+const setExample = new Set([1,2,3,4,5,6]);
+
+// Method 1 using spread opreator
+const array1 = [...setExample];
+console.log(array1)
+
+
+const array2 = Array = Array.from(setExample);
+console.log(array2);
+
+
+
+
+
+//! 10. How do you clear all elements from a Set?
+// Use the `.clear()` method to remove all elements from a Set.
+
+const cities = new Set(["Mumbai", "Delhi", "Pune"]);
+
+console.log(cities.size); 
+
+cities.clear(); 
+
+console.log(cities.size); 
+console.log(cities); 
