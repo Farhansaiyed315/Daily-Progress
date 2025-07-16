1da452089853be47766b0cccf089233b49325043
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-07-16 22:24:54 +0530

    Daily progress

--- Exercise/DOM/Dom1/Domi.js
+++ Exercise/DOM/Dom1/Domi.js
@@ -21,4 +21,35 @@ console.dir(tag)
 
 
 
+// Accessing by query selector. 
+
+let query = document.querySelector("h4");  // returns matching first element
+console.dir(query)
+
+
+
+
+let query = document.querySelectorAll("h4");   // for all matching
+console.dir(query)
+
+
+
+//! Properties 
+
+// TagName :  returns tag for element nodes. 
+
+// innerText: returns the text content of the element and all its children
+
+
+// innerHtml : returns the plain text or HTML contents in the element 
+
+// textContent: returns textual content even hidden elements. 
+
+
+
+
+//! parent child siblings  Important mind mein hai 
+
+//! first child last child children padho jaan kr rakho. 
+
 
