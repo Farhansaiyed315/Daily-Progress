25acb86e9f12ca0175f3bc7d311a324228f9d698
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-07-23 18:27:37 +0530

    Daily progress

---
+++ Projects/filter-search/script.js
@@ -0,0 +1,67 @@
+
+
+let products = {
+    date: [{
+        productName: "Regular white t shirt",
+        category: "Top wear",
+        price: "149",
+        image: "white-shirt.jpg",
+
+    },
+
+    {
+        productName: "Short skirt",
+        category: " Bottom wear",
+        price: "149",
+        image: "short-skirt.jpg"
+    },
+
+    {
+
+        productName: "pants",
+        category: " Bottom wear",
+        price: "149",
+        image: "pants.jpg"
+
+    }, 
+    
+    
+    {
+
+        productName: "jacket",
+        category: " Top wear",
+        price: "349",
+        image: "pants.jpg"
+  
+    },
+    
+    
+    {
+
+        productName: "watch",
+        category: "accessories",
+        price: "549",
+        image: "pants.jpg"
+        
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ]
+}
