a0570936c14684ad79b7966608ba0883050929c4
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-07-28 19:11:39 +0530

    Daily progress

---
+++ Exercise/Advance/fetch/Questions.js
@@ -0,0 +1,50 @@
+
+
+//! 1. What is `fetch()` in JavaScript?
+
+
+
+ //! 2. What are the advantages of using `fetch()` over `XMLHttpRequest`?
+
+
+
+ //! 3. How do you handle errors with `fetch()`?
+
+
+
+ //! 4. How do you make a POST request using `fetch()`?
+
+
+
+ //! 5. What is the difference between using fetch().then() and async/await?
+
+
+
+
+
+
+
+
+
+
+ 
+
+//?  ------------Async - Await ---------------
+
+ //! 1. What is `async/await` in JavaScript?
+
+
+
+ //! 2. What happens if we forget to use `await` inside an async function?
+
+
+
+ //! 3. How do you handle errors in async/await?
+
+
+
+ //! 4. Can we use async/await with multiple fetches?
+
+
+
+ //! 5. What happens if you use `await` outside of an async function?
