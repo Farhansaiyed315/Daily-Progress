0f95b19f2bab1816ab0e45865cf5b916f13de2c5
Farhan Saiyed <saiyedfarhan315@gmail.com>
2025-08-04 18:50:55 +0530

    Daily progress

--- Revision/Array/Questions.js
+++ Revision/Array/Questions.js
@@ -17,6 +17,9 @@
 
 
 //! 5. Declare an array called mixedDataTypes, put different data types in the 
+
+
+
 //!    array and find the length of the array. The array size should be greater than 5
 
 
---
+++ Revision/Array/array1.js
@@ -0,0 +1,56 @@
+//! 1. Declare an empty array
+
+const arr = [" "];
+console.log(typeof(arr));
+
+
+
+
+
+
+//! 2. Declare an array with more than 5 number of elements
+
+const arrname  = ["Farhan", "Faizan", 22, "Blind spot ","Dutch"];
+console.log(arrname);
+
+//! 3. Find the length of your array
+console.log("The lenght of the array is",arrname.length)
+
+
+
+
+
+
+
+
+
+//! 4. Get the first item, the middle item and the last item of the array
+
+const arrname  = ["Farhan", "Faizan", 22, "Blind spot ","Dutch", 2255];
+
+firstItem = arrname[0];
+console.log("firstItem is ", firstItem)
+
+LastItem = arrname[arrname.length-1];
+console.log("The last item is ", LastItem)
+
+MiddleItem = arrname[Math.floor(arrname.length / 2)];
+console.log("The middle item is ", MiddleItem)
+
+
+
+
+
+
+
+
+
+//! 5. Declare an array called mixedDataTypes, put different data types in the 
+
+
+
+
+
+
+
+
